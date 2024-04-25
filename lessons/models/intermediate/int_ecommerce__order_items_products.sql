1bbba4d16964ec565e00fcfc28d2ac035691703e
Mark Garrett <mgarrett2008@gmail.com>
2024-04-24 21:03:31 -0400

    updating models

---
+++ lessons/models/intermediate/int_ecommerce__order_items_products.sql
@@ -0,0 +1,38 @@
+WITH products AS (
+	SELECT
+		product_id,
+		department AS product_department,
+		cost AS product_cost,
+		retail_price AS product_retail_price
+
+	FROM {{ ref('stg_ecommerce__products') }}
+),
+
+order_items as (
+	SELECT *
+
+	FROM {{ ref('stg_ecommerce__order_items') }}
+)
+
+SELECT
+
+	-- IDs
+	order_items.order_item_id,
+	order_items.order_id,
+	order_items.user_id,
+	order_items.product_id,
+
+	-- Order item data
+	order_items.item_sale_price,
+
+	-- Product data
+	products.product_department,
+	products.product_cost,
+	products.product_retail_price,
+
+	-- Calculated fields
+	order_items.item_sale_price - products.product_cost AS item_profit,
+	products.product_retail_price - order_items.item_sale_price AS item_discount
+
+FROM order_items
+LEFT JOIN products ON order_items.product_id = products.product_id
